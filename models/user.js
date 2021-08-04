6932a18ee4c05857218378db2051598b272c96bc
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-08-03 21:35:34 -0400

    server config

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose  = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: String,
+        email: {
+            type: String,
+            required: true,
+            index: true,
+        },
+        role: {
+            type: String,
+            default: "subscriber",
+        },
+        cart: {
+            type: Array,
+            default: [],
+        },
+        address: String,
+        // whishlist: [{ type: ObjectId, ref: "Product" }],
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
