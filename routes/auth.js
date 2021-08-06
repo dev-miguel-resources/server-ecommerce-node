e59487be7f0246df62010a74b4abe17ca3b40164
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-08-06 09:42:41 -0400

    endpoints rest, middlewares and controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,17 @@
 const express = require("express");
 
-const router = express.Router();
+const router = express.Router(); 
 
-// middlewares
-//const { authCheck, adminCheck } = require("../middlewares/auth");
+// middlewares: validations
+const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controller
-//const { createOrUpdateUser } = require("../controllers/auth");
+const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 
-// endpoints
-//router.post("/create-or-update-user", authCheck, createOrUpdateUser);
-//router.post("/current-admin", authCheck, adminCheck, currentUser);
+// endpoints routes
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+router.post("/current-user", authCheck, currentUser);
+router.post("/current-admin", authCheck, adminCheck, currentUser);
 
 module.exports = router;
+
